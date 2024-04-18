f5a9549eb7f8fd30ed83263d780fd95afddd9264
Ana Clara Bertello <bertelloana@outlook.com>
2024-04-17 22:32:16 -0300

    Creación Clases Marca/Categoria

---
+++ WindowsForms/Brand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms
+{
+    internal class Brand
+    {
+        private int Id;
+        public int BrandId
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+
+
+        private string Name;
+        public string BrandName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+       }
+    }
+
---
+++ WindowsForms/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WindowsForms
+{
+    internal class Category
+    {
+
+        private int Id;
+        public int CategoryId
+        { 
+        get { return Id; }
+        set { Id = value;}
+        }
+
+        private string Name;
+        public string CategoryName
+        {
+        get { return Name; }
+        set { Name =value;}
+        }
+    }
+}
